5948d34db71926fd9490e358aeb512bb41e2bc5a
saturnette <javerflors@gmail.com>
2024-08-04 21:42:35 -0600

    xd

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+  type        = string
+  default     = "commette"
+  description = "the name project"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "the name environment"
+}
+
+variable "location" {
+  type        = string
+  default     = "East US 2"
+  description = "the location of the resources"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {
+    environment = "dev"
+    project     = "commette"
+    created_by  = "Terraform"
+  }
+  description = "the tags for the resources"
+}
