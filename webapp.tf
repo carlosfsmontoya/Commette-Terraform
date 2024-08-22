38300d16b339dc059dc45f708b2579b492d2d609
saturnette <javerflors@gmail.com>
2024-08-22 07:38:56 -0600

    added webapp3

--- webapp.tf
+++ webapp.tf
@@ -97,3 +97,40 @@ resource "azurerm_app_service_virtual_network_swift_connection" "webapp2_vnet_in
   ]
 
 }
+
+resource "azurerm_app_service" "webapp3" {
+  name                = "go-${var.project}-${var.environment}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/${var.project}/go:latest"
+    always_on        = true
+    vnet_route_all_enabled = true
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
+    "WEBSITE_VNET_ROUTE_ALL"          = "1"
+  }
+
+  depends_on = [
+    azurerm_app_service_plan.app_service_plan,
+    azurerm_container_registry.acr,
+    azurerm_subnet.subnetweb
+  ]
+
+  tags = var.tags
+}
+
+resource "azurerm_app_service_virtual_network_swift_connection" "webapp3_vnet_integration" {
+  app_service_id    = azurerm_app_service.webapp3.id
+  subnet_id         = azurerm_subnet.subnetweb.id
+
+  depends_on = [
+    azurerm_app_service.webapp3
+  ]
+}
